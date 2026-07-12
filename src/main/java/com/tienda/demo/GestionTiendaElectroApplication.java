cd18dfd75571218e67f931a94cac0743517e8d0d
Manuel Romano <mromano.dev@gmail.com>
2026-07-12 02:39:19 +0200

    Primer commit

---
+++ src/main/java/com/tienda/demo/GestionTiendaElectroApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTiendaElectroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTiendaElectroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/demo/GestionTiendaElectroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTiendaElectroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
